714062b3e10d55bf490760414677140be6c7359c
Md Adil <md-adil@live.com>
2023-08-28 23:41:54 +0530

    refactor cjs poly

--- src/plugins/cjs-polyfill.ts
+++ src/plugins/cjs-polyfill.ts
@@ -1,34 +1,37 @@
-import type { Plugin } from "esbuild";
+import type { Plugin, PluginBuild } from "esbuild";
+
+function setup({ onLoad, onResolve, initialOptions }: PluginBuild) {
+  const namespace = "cjs-polyfill";
+  if (!initialOptions.inject) {
+    initialOptions.inject = [];
+  }
+  initialOptions.inject.push("cjs:virtual");
+  onResolve({ filter: /^cjs:virtual$/ }, (args) => {
+    return {
+      path: args.path,
+      namespace,
+    };
+  });
+  onLoad({ filter: /.*/, namespace }, () => {
+    return {
+      loader: "js",
+      contents: `import { URL, fileURLToPath } from 'node:url';
+      import { dirname } from 'node:path';
+      import { createRequire } from 'node:module';
+      const currentModuleURL = new URL(import.meta.url);
+      const currentModulePath = fileURLToPath(currentModuleURL);
+      const currentModuleDirname = dirname(currentModulePath);
+      globalThis.__filename = currentModulePath;
+      globalThis.__dirname = currentModuleDirname;
+      globalThis.require = createRequire(import.meta.url);
+      `,
+    };
+  });
+}
+
 export function cjs(): Plugin {
   return {
     name: "cjs-polyfill",
-    setup({ onLoad, onResolve, initialOptions }) {
-      const namespace = "cjs-polyfill";
-      if (!initialOptions.inject) {
-        initialOptions.inject = [];
-      }
-      initialOptions.inject.push("cjs:virtual");
-      onResolve({ filter: /^cjs:virtual$/ }, (args) => {
-        return {
-          path: args.path,
-          namespace,
-        };
-      });
-      onLoad({ filter: /.*/, namespace }, () => {
-        return {
-          loader: "js",
-          contents: `import { URL, fileURLToPath } from 'node:url';
-          import { dirname } from 'node:path';
-          import { createRequire } from 'node:module';
-          const currentModuleURL = new URL(import.meta.url);
-          const currentModulePath = fileURLToPath(currentModuleURL);
-          const currentModuleDirname = dirname(currentModulePath);
-          globalThis.__filename = currentModulePath;
-          globalThis.__dirname = currentModuleDirname;
-          globalThis.require = createRequire(import.meta.url);
-          `,
-        };
-      });
-    },
+    setup,
   };
 }
